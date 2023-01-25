8c5c494078b91f143ddd9ae6c767f72c5a6bc54d
Edward Morgan <emorg31@lsu.edu>
2023-01-24 19:40:21 -0600

    include servo cycle time

--- src/pid_control.py
+++ src/pid_control.py
@@ -7,27 +7,26 @@ class PID():
         self.kd = kd
 
         self.iTerm = 0
-        self.dt = 0.001
 
+        self.tprev = 0
+
+        self.dt = 0.001 #servo cycle time
         self.time = 0
-
-        self.qprev = self.senseAngle()
+        self.qprev = self.senseAngle() # initial joint angle q
         self.windupMax = windupMax
 
-    def controller(self):
-        [qd,qdotd] = self.trajectory(self.time) # get desired angle and desired velocity
-        q = self.senseAngle()
+    def controller(self, time):
+        [qd,qdotd] = self.trajectory(time) #get desired angle and desired velocity
+        q = self.senseAngle() # sense actual joint angle
         qdot = (q - self.qprev)/self.dt
         q = self.qprev
 
         e = qd - q
         edot = qdotd - qdot
 
-        eint = eint + e*self.dt
+        eint = eint + e*self.dt #integral error
         tau = self.kp*e + self.kd*edot + self.ki*eint
-
         self.time = self.time + self.dt
-
         return tau
 
         
